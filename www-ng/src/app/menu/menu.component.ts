947decb34e3ca0efe01049b5c4166602414178a4
andregomars <andregomars@gmail.com>
2017-03-09 00:38:23 -0800

    fix menu

--- www-ng/src/app/menu/menu.component.ts
+++ www-ng/src/app/menu/menu.component.ts
@@ -13,57 +13,32 @@ export class MenuComponent implements OnInit {
 
     ngOnInit() {
       this.menuItems = [
-                {
-                    label: 'BYD', 
-                    url: '/fleet',
-                    items: [
-                      [
-                            {
-                                label: 'AZ01',
-                                routerLink: '/vehicle/AZ01'
-                            },
-                            {
-                                label: 'AZ02',
-                                routerLink: '/vehicle/AZ02'
-                            }
-                         ] ]
-                },
-                {
-                    label: 'Sports', icon: 'fa-soccer-ball-o',
-                    items: [
-                        [
-                            {
-                                label: 'Sports 1',
-                                items: [{label: 'Sports 1.1'},{label: 'Sports 1.2'}]
-                            },
-                            {
-                                label: 'Sports 2',
-                                items: [{label: 'Sports 2.1'},{label: 'Sports 2.2'}]
-                            },
-
-                        ],
-                        [
-                            {
-                                label: 'Sports 3',
-                                items: [{label: 'Sports 3.1'},{label: 'Sports 3.2'}]
-                            },
-                            {
-                                label: 'Sports 4',
-                                items: [{label: 'Sports 4.1'},{label: 'Sports 4.2'}]
-                            }
-                        ],
-                        [
-                            {
-                                label: 'Sports 5',
-                                items: [{label: 'Sports 5.1'},{label: 'Sports 5.2'}]
-                            },
-                            {
-                                label: 'Sports 6',
-                                items: [{label: 'Sports 6.1'},{label: 'Sports 6.2'}]
-                            }
-                        ]
-                    ]
-                }
-            ];
+            {
+                label: 'LBT',
+                routerLink: ['/fleet'],
+                items: [
+                    { 
+                      label: 'AZ01',
+                      routerLink: ['/vehicle', 'AZ01'],
+                    }, { 
+                      label: 'AZ02',
+                      routerLink: ['/vehicle', 'AZ02'],
+                    }
+                ]
+            },
+            {
+                label: 'BYD',
+                routerLink: ['/fleet'],
+                items: [
+                    { 
+                      label: 'P01',
+                      routerLink: ['/vehicle', 'P01'],
+                    }, { 
+                      label: 'P02',
+                      routerLink: ['/vehicle', 'P02'],
+                    }
+                ]
+            }
+        ];
     }
 }
