601118a21f83274cb82696043b0a345c68e14657
andregomars <andregomars@gmail.com>
2017-11-12 20:46:30 -0800

    clean unused data

--- www-test/db-test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbuser = 'wp';
-$dbpass = 'wp';
-$dbhost = 'mysql.demo';
-
-$connect = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
-
-echo "Connected to DB!";
